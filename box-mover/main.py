e9cb20074ef6b5c4d9d2f8eebc800bfcd86b1fe7
FakePotatoGuy <noah.j.naylor.1@gmail.com>
2026-08-19 12:01:35 -0600

    finnished setup

--- box-mover/main.py
+++ box-mover/main.py
@@ -1,3 +1,18 @@
 import pygame
 
+screen_hight=600
+screen_width=700
+
 pygame.init()
+screen=pygame.display.set_mode((screen_width,screen_hight))
+pygame.display.set_caption("Box Mover")
+clock=pygame.time.Clock()
+
+running=True
+while running:
+    for event in pygame.event.get():
+        if event.type==pygame.QUIT:
+            running=False
+
+    clock.tick(60)
+pygame.quit()
