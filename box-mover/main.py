9f2a8e9485e5e6dc7dd23fb9f4b165a6f9666e2a
FakePotatoGuy <noah.j.naylor.1@gmail.com>
2026-08-19 12:26:44 -0600

    added bg

--- box-mover/main.py
+++ box-mover/main.py
@@ -1,18 +1,27 @@
 import pygame
 
-screen_hight=600
-screen_width=700
+screen_hight=590
+screen_width=690
 
 pygame.init()
 screen=pygame.display.set_mode((screen_width,screen_hight))
 pygame.display.set_caption("Box Mover")
 clock=pygame.time.Clock()
 
+bg=pygame.image.load("assets/images/bg.png")
+
+def draw():
+    screen.fill("black")
+    screen.blit(bg)
+    pygame.display.flip()
+
 running=True
 while running:
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
             running=False
+
+    draw()
 
     clock.tick(60)
 pygame.quit()
