e2b871c6629f4876b955773f42c027821b8a8d84
FakePotatoGuy <noah.j.naylor.1@gmail.com>
2026-08-28 13:24:11 -0600

    t

--- swift'n'simple/main.py
+++ swift'n'simple/main.py
@@ -1,1 +1,18 @@
 import pygame
+
+screen_width=500
+screen_height=600
+
+pygame.init()
+screen=pygame.display.set_mode(())
+pygame.display.set_caption("swift'n'simple")
+clock=pygame.time.Clock()
+
+running=True
+while running:
+    for event in pygame.event.get():
+        if event.type==pygame.QUIT:
+            running=False
+
+    clock.tick(60)
+pygame.quit()
